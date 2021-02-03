13ea32bda9847ed8037d531c726d2ca90c23c973
devanshu06 <devuvns@gmail.com>
2021-02-03 20:57:14 +0530

    commit on master

---
+++ DSA_Algo/LinearSearch.py
@@ -0,0 +1,20 @@
+#UNORDERED LIST
+#random, not fixed length
+x = [10, 1, 5, 93, 51, 7, 16]
+
+#Algo: LinearSearch or SequentialSearch
+def SequentialSearch(arr, item):
+    position = 0 
+    found = False
+
+    while position < len(arr) and not found:
+        if x[position] == item:
+            found = True
+            print("Found the Item.")
+        else:
+            position+=1
+            
+    return found
+
+
+SequentialSearch(x, 1)
