d9506aa80b6c7e1c5a753826b235fb451918781a
devanshu06 <devuvns@gmail.com>
2021-02-07 19:20:53 +0530

    Second commit on the BubbleSort

--- DSA_Algo/BubbleSort.py
+++ DSA_Algo/BubbleSort.py
@@ -1,14 +1,17 @@
-def BubbleSort(data):
-    for f in range(len(data)-1):
-        print("First Data: ", f)
-        for s in range(len(data)-1-f):
-            print("Second data: ", s)
-            if data[s] > data[s+1]:
+################################################################################## BUBBLE SORT #######################################################################
+def BubbleSort(data):        
+    swap = False        # Creating a Swap Varaiable 
+    for f in range(len(data)-1):                 # getting all the value of data in the f variable
+        for s in range(len(data) - 1 - f):       # creting the S variable for comparing the data                 
+            if data[s] > data[s+1]:              
                 data[s], data[s+1] = data[s+1], data[s]
+                swap == True                
                 print("Swap Done")
-        print("Pass Completed")
-        print("Every Pass result: ", data)
-    return data 
+                print("The data after swapping: ", data)
+            if swap == False:    
+                print("The Sorted data: ", data)
 
-db = [10, 55, 94, 13, 58]
+db = [25,69,83,512,54,67,12,53]
 BubbleSort(db)
+
+
